2c080dbec92d4601b1ce264ebb28bdb35e5b2887
Umar Mukhtar <umar@tuffah.info>
2015-12-11 04:22:54 +0800

    ready to slave

--- src/models/DBConn.java
+++ src/models/DBConn.java
@@ -20,7 +20,7 @@ public class DBConn {
     private static String pass = "qwerty";
     private static String database = "servercis";
     private static String dbUrl = "jdbc:mysql://" + getHost() + "/" + getDatabase();
-    private static int port_rmi = 1098;
+    private static int port_rmi = 1099;
 
     public static String getHost() {
         return host;
